fe1d946702b1b466aece7b49457e97fa792eaeed
Steve Widinghoff <steve.widinghoff@storytel.com>
2019-05-09 22:10:46 +0200

    Initial commit

---
+++ src/main/java/com/abrovinc/tugboat/TugboatApplication.java
@@ -0,0 +1,9 @@
+package com.abrovinc.tugboat;
+
+
+public class TugboatApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/test/java/com/abrovinc/tugboat/TugboatApplicationTests.java
@@ -0,0 +1,12 @@
+package com.abrovinc.tugboat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class TugboatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
